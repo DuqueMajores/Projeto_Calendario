bc04c3352cf9e146122e490dd312a7970d50199c
DuqueMajores <113138671+DuqueMajores@users.noreply.github.com>
2024-05-25 09:20:41 -0300

    Add files via upload

---
+++ Projeto Calendário/script.js
@@ -0,0 +1,75 @@
+function clicou(){
+    var index = "index.html";
+    var url1 = "janeiro.html";
+    var url2 = "fevereiro.html";
+    var url3 = "marco.html";
+    var url4 = "abril.html";
+    var url5 = "maio.html";
+    var url6 = "junho.html";
+    var url7 = "julho.html";
+    var url8 = "agosto.html";
+    var url9 = "setembro.html";
+    var url10 = "outubro.html";
+    var url11 = "novembro.html";
+    var url12 = "dezembro.html";
+    var url13 = "todos.html"
+
+
+    var inpute = document.getElementById("input").value;
+
+    if ('indexIndex'.includes(inpute)){
+        window.location.href = index;
+    }
+
+    if ('Janeirojaneiro'.includes(inpute)){
+        window.location.href = url1;
+    }
+
+    if ('Fevereirofevereiro'.includes(inpute)){
+        window.location.href = url2;
+    }
+
+    if('Marçomarço'.includes(inpute)){
+        window.location.href = url3;
+    }
+
+    if('Abrilabril'.includes(inpute)){
+        window.location.href = url4;
+    }
+
+    if ('Maiomaio'.includes(inpute)){
+        window.location.href = url5
+    }
+
+    if('Junhojunho'.includes(inpute)){
+        window.location.href = url6;
+    }
+
+    if('Julhojulho'.includes(inpute)){
+        window.location.href = url7;
+    }
+
+    if('Agostoagosto'.includes(inpute)){
+        window.location.href = url8;
+    }
+
+    if('Setembrosetembro'.includes(inpute)){
+        window.location.href = url9;
+    }
+
+    if ('Outubrooutubro'.includes(inpute)){
+        window.location.href = url10;
+    }
+
+    if ('Novembronovembro'.includes(inpute)){
+        window.location.href = url11;
+    }
+
+    if ('Dezembrodezembro'.includes(inpute)){
+        window.location.href = url12;
+    }
+
+    if ('Todostodos'.includes(inpute)){
+        window.location.href = url13;
+    }
+}
